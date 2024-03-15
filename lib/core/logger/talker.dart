35d28506265794054a90edfba34f1787c9fa0c1b
Arin Faraj <arin.abdul99@gmail.com>
2024-03-15 03:01:07 +0300

    add another form of logging

--- lib/core/logger/talker.dart
+++ lib/core/logger/talker.dart
@@ -1,3 +1,34 @@
-import 'package:talker/talker.dart';
+import 'package:simple_logger/simple_logger.dart';
+// import 'package:talker/talker.dart';
+// final logger = Talker(
+//   settings: TalkerSettings(),
+// );
 
-final logger = Talker();
+final logger = SimpleLogger()
+  ..setLevel(
+    Level.FINEST,
+    includeCallerInfo: false,
+  )
+  ..formatter = (info) => '${_levelInfo(info.level)}'
+      '${info.message}';
+
+String _levelInfo(Level level) {
+  return '${levelPrefixes[level] ?? ''}$level ';
+}
+
+Map<Level, String> levelPrefixes = {
+  Level.FINEST: '👾 ',
+  Level.FINER: '👀 ',
+  Level.FINE: '🎾 ',
+  Level.CONFIG: '🐶 ',
+  Level.INFO: '👻 ',
+  Level.WARNING: '⚠️ ',
+  Level.SEVERE: '‼️ ',
+  Level.SHOUT: '😡 ',
+};
+
+extension ExLogger on SimpleLogger {
+  void verbose(String message) {
+    finest(message);
+  }
+}
