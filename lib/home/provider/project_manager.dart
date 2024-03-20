9441968c3d2fcc5041afd354a4c111702133a9e7
Arin Faraj <arin.abdul99@gmail.com>
2024-03-20 04:19:34 +0300

    improve project manager

--- lib/home/provider/project_manager.dart
+++ lib/home/provider/project_manager.dart
@@ -1,42 +1,57 @@
 import 'dart:io';
 
 import 'package:i18n_editor/core/settings/recent_projects.dart';
+import 'package:i18n_editor/core/toast/toast_provider.dart';
+import 'package:i18n_editor/home/provider/i18n_configs_provider.dart';
 import 'package:riverpod/riverpod.dart';
 
 typedef Project = String;
 
 class ProjectManagerNotifier extends Notifier<Project?> {
   @override
-  Project? build() {
-    return null;
-  }
+  Project? build() => null;
 
-  void openProject(String dirPath) async {
-    Directory dir = Directory(dirPath);
+  void openProject(String project) async {
+    final recentProjects = List<String>.from(
+      ref.read(recentProjectsProvider).requireValue,
+    );
 
-    final newRecentProjects =
-        List<String>.from(ref.read(recentProjectsProvider).requireValue)
-          ..remove(dirPath)
-          ..insert(0, dirPath);
-
-    if (await dir.exists()) {
-      state = dirPath;
+    if (await Directory(project).exists()) {
+      state = project;
+      recentProjects
+        ..remove(project)
+        ..insert(0, project);
     } else {
-      newRecentProjects.remove(dirPath);
+      ref.read(toastProvider.notifier).show(
+            'Directory does not exist',
+            type: ToastType.warning,
+          );
+      recentProjects.remove(project);
     }
 
-    ref.read(recentProjectsProvider.notifier).set(newRecentProjects);
+    ref.read(recentProjectsProvider.notifier).set(recentProjects);
   }
 
-  void closeProject() {
+  Future<void> closeProject() async {
+    if (state == null) return;
+    final oldState = state;
+    final i18nConfigs = await ref.read(i18nConfigsProvider.future);
     state = null;
+
+    if (i18nConfigs == null) {
+      final recentProjects = List<String>.from(
+        ref.read(recentProjectsProvider).requireValue,
+      );
+
+      recentProjects.remove(oldState);
+
+      ref.read(recentProjectsProvider.notifier).set(recentProjects);
+    }
   }
 }
 
 final projectManagerProvider =
     NotifierProvider<ProjectManagerNotifier, Project?>(
-  () {
-    return ProjectManagerNotifier();
-  },
+  ProjectManagerNotifier.new,
   name: 'projectManager',
 );
